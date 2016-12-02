03513dcfb6dff6cf4dc729b0cb023c6d33882ca8
qualious <denizsydgl@gmail.com>
2016-12-03 01:06:24 +0300

    Changed the way we connect to the database

--- registration.php
+++ registration.php
@@ -1,32 +1,33 @@
 <html>
-	<head>
-		<title>Information Gathered</title>
-	</head>
-	<body>
+    <head>
+        <title>Information Gathered</title>
+    </head>
+    <body>
 
     <?php
         $servername = "localhost";
         $uname = "root";
         $password = "password";
 
-        // Create connection
-        $conn = new mysqli($servername, $uname, $password);
+        $dbhandle = mysql_connect($servername, $uname, $password) 
+            or die("Unable to connect to MySQL");
+        echo "Connected to MySQL<br>";
+        
+        $user_db = mysql_select_db("User",$dbhandle) 
+            or die("Could not select user");
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
 
-    	$userName = $_POST['username'];
-    	$password1 = $_POST['password1'];
-    	$password2= $_POST['password2'];
-    	if($password1 != $password2)
-    		echo "Passwords don't match!";
-    	else
-    		echo "OK";
+        $userName = $_POST['username'];
+        $password1 = $_POST['password1'];
+        $password2= $_POST['password2'];
+        if($password1 != $password2)
+            echo "Passwords don't match!";
+        else
+            echo "OK";
 
         $query = "SELECT * from User where username=$userName"
-        if(mysql_query($query)){
+        $result = mysql_query($query);
+        if(!$result){
             echo "Username $userName already exists, please try again!";
         }else{
             $query2 = "INSERT into User (username, password) VALUES ($userName, $password1)"
@@ -36,5 +37,5 @@
 
     ?>
 
-	</body>
+    </body>
 </html>
