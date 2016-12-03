504dd0206611396723dbbb2c95d36be03c706a04
qualious <denizsydgl@gmail.com>
2016-12-03 13:17:33 +0300

    Updated regostraton.php as discussed.
    I'm not really sure about implementation, we can try after we created a
    database.

--- registration.php
+++ registration.php
@@ -1,41 +1,64 @@
 <html>
     <head>
-        <title>Information Gathered</title>
+        <title>Processing information...</title>
     </head>
     <body>
 
     <?php
-        $servername = "localhost";
-        $uname = "root";
-        $password = "password";
+        $conn = new mysqli("localhost", "root", "", "password");
 
-        $dbhandle = mysqli_connect($servername, $uname, $password) 
-            or die("Unable to connect to MySQL");
-        echo "Connected to MySQL<br>";
-        
-        $user_db = mysqli_select_db("User",$dbhandle) 
-            or die("Could not select user");
-
+        if($conn->connect_error){
+            die("Connection failed: " . $conn->connect_error);
+        }
 
         $userName = $_POST['username'];
         $password1 = $_POST['password1'];
         $password2= $_POST['password2'];
-        if($password1 != $password2)
+        if($password1 != $password2){
             echo "Passwords don't match!";
+            //if passwords don't match, send user to DBProject.html
+            <script>
+                window.setTimeout(function () {
+                    window.location = "DBProject.html";
+                },2000);
+            </script>
+        }
         else
             echo "OK";
 
-        $query = "SELECT * FROM User where username=$userName";
-        $result = mysql_query($query);
-        if($result){
+        $query = "SELECT * from User where username=$userName";
+        if($conn->query($query) == TRUE){
+            //if username exists, send user to DBProject.html
             echo "Username $userName already exists, please try again!";
+            <script>
+                window.setTimeout(function () {
+                    window.location = "DBProject.html";
+                },3000);
+            </script>
         }else{
-            $query2 = "INSERT INTO User (username, password) VALUES ($userName, $password1)";
-            mysql_query($query2);
-            echo "User sucesfully created.";
+            $query2 = "INSERT into User (username, password) VALUES ($userName, $password1)";
+            if($conn->query($query2) === TRUE){
+                echo "Registration complete";
+                //if registration is complete, send user to logged.html
+                <script>
+                    window.setTimeout(function () {
+                        window.location = "logged.html";
+                    },2000);
+                </script>
+            }
+            else{
+                echo "Connection error " . $conn->error;
+                //if connection error occurs, send user to DBProject.html
+                <script>
+                    window.setTimeout(function () {
+                        window.location = "DBProject.html";
+                    },2000);
+                </script>
+            }
         }
-
+    $conn->close();
     ?>
 
     </body>
 </html>
+
