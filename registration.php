22afcce38773d08f502960d2cff96b402f139c6a
qualious <denizsydgl@gmail.com>
2016-12-22 21:32:26 +0300

    Changed script bits to php

--- registration.php
+++ registration.php
@@ -5,7 +5,7 @@
     <body>
 
     <?php
-        $conn = new mysqli("localhost", "root", "", "password");
+        $conn = new mysqli("localhost", "root", "", "oldrapper");
 
         if($conn->connect_error){
             die("Connection failed: " . $conn->connect_error);
@@ -16,44 +16,55 @@
         $password2= $_POST['password2'];
         if($password1 != $password2){
             echo "Passwords don't match!";
-            //if passwords don't match, send user to DBProject.html
-            <script>
-                window.setTimeout(function () {
-                    window.location = "DBProject.html";
-                },2000);
-            </script>
+            //if passwords don't match, send user to index.html
+            // <script>
+            //     window.setTimeout(function () {
+            //         window.location = "index.html";
+            //     },2000);
+            // </script>
+            header('Location: index.html');
+			exit;
+
         }
         else
             echo "OK";
 
         $query = "SELECT * from User where username=$userName";
         if($conn->query($query) == TRUE){
-            //if username exists, send user to DBProject.html
+            //if username exists, send user to index.html
             echo "Username $userName already exists, please try again!";
-            <script>
-                window.setTimeout(function () {
-                    window.location = "DBProject.html";
-                },3000);
-            </script>
+            // <script>
+            //     window.setTimeout(function () {
+            //         window.location = "index.html";
+            //     },3000);
+            // </script>
+            header('Location: index.html');
+			exit;
         }else{
             $query2 = "INSERT into User (username, password) VALUES ($userName, $password1)";
             if($conn->query($query2) === TRUE){
                 echo "Registration complete";
                 //if registration is complete, send user to logged.html
-                <script>
-                    window.setTimeout(function () {
-                        window.location = "logged.html";
-                    },2000);
-                </script>
+                // <script>
+                //     window.setTimeout(function () {
+                //         window.location = "logged.html";
+                //     },2000);
+                // </script>
+                header('Location: logged.html');
+				exit;
+
             }
             else{
                 echo "Connection error " . $conn->error;
-                //if connection error occurs, send user to DBProject.html
-                <script>
-                    window.setTimeout(function () {
-                        window.location = "DBProject.html";
-                    },2000);
-                </script>
+                //if connection error occurs, send user to index.html
+                // <script>
+                //     window.setTimeout(function () {
+                //         window.location = "index.html";
+                //     },2000);
+                // </script>
+                header('Location: index.html');
+				exit;
+
             }
         }
     $conn->close();
